e62882a6f229cf9e3a1f7f23517c97bfb3734395
seelamne <seelamne@msu.edu>
2023-10-20 18:16:49 -0400

    Created Face recognition app cmake project

---
+++ FaceRecognitionApp/FaceRecognitionApp.cpp
@@ -0,0 +1,12 @@
+﻿// FaceRecognitionApp.cpp : Defines the entry point for the application.
+//
+
+#include "FaceRecognitionApp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
